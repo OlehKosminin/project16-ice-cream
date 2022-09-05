5494170c0eb9bb6b854e0528133d27f7e95afb5d
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2022-09-05 20:34:25 +0300

    slider
    Йобаный пиздец

---
+++ src/js/script-slider.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
