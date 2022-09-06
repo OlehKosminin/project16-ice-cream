b50221b2544545dfefbe2e4b437580ee44fcdde0
Olehys <arzamazova13@gmail.com>
2022-09-06 22:44:49 +0300

    add chench

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 
 import ('./js/mobile-menu.js')
 import('./js/modal-buy-now.js')
-import(`./js/contact-locations`)
-import(`./js/contact-franchise`)
+import(`./js/contact-locations.js`)
+import(`./js/contact-franchise.js`)
+import(`./js/script-slider`)
+
 
