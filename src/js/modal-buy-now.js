2d9acb762be8ab9a99b95952a07804134f24a654
ertyhd <orionship@protonmail.com>
2022-09-06 21:49:30 +0300

    changes in modal window js

--- src/js/modal-buy-now.js
+++ src/js/modal-buy-now.js
@@ -1,8 +1,8 @@
 (() => {
     const refs = {
-    openModalBtn: document.querySelectorAll(".click"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelectorAll(".click-buy-now"),
+    closeModalBtn: document.querySelector("[data-modal-close-buy-now]"),
+    modal: document.querySelector("[data-modal-buy-now]"),
     body: document.querySelector('body'),
     };
     refs.openModalBtn.forEach(item => {
@@ -12,7 +12,7 @@
     refs.closeModalBtn.addEventListener('click', toggleModal);
     
     function toggleModal() {
-        refs.modal.classList.toggle('is-hidden');
+        refs.modal.classList.toggle('is-hidden-buy-now');
         // refs.body.classList.toggle('no-scroll');
     }
 })();
