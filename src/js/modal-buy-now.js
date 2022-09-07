414a199cf711f2f4a4bd8c241e22d8788f7ff280
ertyhd <orionship@protonmail.com>
2022-09-07 12:08:40 +0300

    some fix

--- src/js/modal-buy-now.js
+++ src/js/modal-buy-now.js
@@ -1,18 +1,12 @@
 (() => {
     const refs = {
-    openModalBtn: document.querySelectorAll(".click-buy-now"),
+    openModalBtn: document.querySelector("[data-modal-open-buy-now]"),
     closeModalBtn: document.querySelector("[data-modal-close-buy-now]"),
     modal: document.querySelector("[data-modal-buy-now]"),
-    body: document.querySelector('body'),
     };
-    refs.openModalBtn.forEach(item => {
-        item.addEventListener('click', toggleModal);
-    });
-    
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-    
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
     function toggleModal() {
-        refs.modal.classList.toggle('is-hidden-buy-now');
-        // refs.body.classList.toggle('no-scroll');
+    refs.modal.classList.toggle("is-hidden-buy-now");
     }
 })();
