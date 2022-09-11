6e3e9b27ec1352036a1bd929e3f1e49abb2f0fd4
OlehKosminin <106763163+OlehKosminin@users.noreply.github.com>
2022-09-11 07:43:42 +0000

    Deploying to gh-pages from @ OlehKosminin/project16-ice-cream@e43c0aeb29898dddfb282f2df25ee8fed0bccc46 🚀

---
+++ index.2ac29e0f.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".mySwiper",{spaceBetween:30,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.2ac29e0f.js.map
