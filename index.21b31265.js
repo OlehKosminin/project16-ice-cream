06d544a841b62b652f99f74fdfeaec107bb0b2ac
OlehKosminin <106763163+OlehKosminin@users.noreply.github.com>
2022-09-12 18:43:23 +0000

    Deploying to gh-pages from @ OlehKosminin/project16-ice-cream@01bff72622f327b6aae2579a5808575bc9f7da1f 🚀

--- index.21b31265.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-open-buy-now]"),closeModalBtn:document.querySelector("[data-modal-close-buy-now]"),modal:document.querySelector("[data-modal-buy-now]")};function e(){o.modal.classList.toggle("is-hidden-buy-now")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.21b31265.js.map
